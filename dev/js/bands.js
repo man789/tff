771fa2d795911bb1db3ecfabe574006bd07b97fa
PierrePsome <pierreterwangne@hotmail.Com>
2022-05-01 23:52:54 +0200

    retouche du menu nav

--- dev/js/bands.js
+++ dev/js/bands.js
@@ -2,6 +2,7 @@ let bandNames = document.querySelectorAll('.band-name');
 let boxImg = document.querySelector('.box-img');
 let bandsBg = document.querySelector('.bands-bg');
 let genre = document.querySelectorAll('.genre');
+let navLogo = document.getElementById('logo-nav');
 
 let a = 0;
 
@@ -10,7 +11,11 @@ function nameOver(param) {
     bandsBg.classList.add('band0' + a);
     bandNames[a].style.transition = ".3s"
     bandNames[a].style.paddingLeft = "10px"
-    bandsBg.style.opacity = 1;
+    genre[0].style.opacity = "0"
+    setTimeout(() => {
+        
+        bandsBg.style.opacity = 1;
+    }, 500);
 }
 
 function nameOut(param) {
@@ -18,4 +23,21 @@ function nameOut(param) {
     bandsBg.classList.remove('band0' + a);
     bandNames[a].style.paddingLeft = "0px"
     bandsBg.style.opacity = 0;
+    genre[0].style.opacity = "1"
 }
+
+// Menu qui réduit onScroll
+window.addEventListener('scroll', function () {
+    let scroll = window.scrollY
+    if (scroll != 0) {
+        navLogo.style.opacity = "0"
+        navLogo.style.width = "0%"
+    } else {
+        setTimeout(() => {
+            navLogo.style.opacity = "1"
+            
+        }, 300);
+        navLogo.style.width = "30%"
+    }
+})
+
