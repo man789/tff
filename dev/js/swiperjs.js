1365daf76e5eb54e103b800209f71085db4f9cbc
duclos pierre <duclospi@gmail.com>
2022-05-04 08:50:00 +0200

    opti swiper

--- dev/js/swiperjs.js
+++ dev/js/swiperjs.js
@@ -1,21 +1,8 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.swiper-aside', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
+    autoplay: {
+      delay: 2500,
     },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+});
