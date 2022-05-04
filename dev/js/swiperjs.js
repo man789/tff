6993e59a028962a142efbc10db0d5b807cdaaab0
manu horta <manuelferreirahorta@gmail.com>
2022-05-04 09:47:18 +0200

    variable

--- dev/js/swiperjs.js
+++ dev/js/swiperjs.js
@@ -6,3 +6,17 @@ const swiper = new Swiper('.swiper-aside', {
       delay: 2500,
     },
 });
+
+const swiper_home = new Swiper('.swiper_home', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
