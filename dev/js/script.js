8c4133637805f4990c0c25b6b08a95a95b92c4cb
PierrePsome <pierreterwangne@hotmail.Com>
2022-05-05 20:58:30 +0200

    structure et css de la homepage

--- dev/js/script.js
+++ dev/js/script.js
@@ -1,18 +1,36 @@
 let navLogo = document.getElementById('logo-nav');
+let header = document.querySelector('.header');
 
 // Menu qui réduit onScroll
 window.addEventListener('scroll', function () {
     let scroll = window.scrollY
     if (scroll != 0) {
         navLogo.style.opacity = "0"
-        navLogo.style.width = "0%"
+        header.style.height = "7%"
     } else {
+        header.style.height = "12%"
         setTimeout(() => {
             navLogo.style.opacity = "1"
             
         }, 300);
-        navLogo.style.width = "30%"
     }
 })
 
+header.addEventListener('mouseenter', function () {
+    let scroll = window.scrollY
+    if (scroll != 0) {
+        header.style.height = "12%"
+        setTimeout(() => {
+            navLogo.style.opacity = "1"
+            
+        }, 300);
+    }
+})
 
+header.addEventListener('mouseleave', function () {
+    let scroll = window.scrollY
+    if (scroll != 0) {
+        navLogo.style.opacity = "0"
+        header.style.height = "7%"
+    }
+})
