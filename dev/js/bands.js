df4d7f12b126b8495406727faa8f130fd3d599b8
PierrePsome <pierreterwangne@hotmail.Com>
2022-05-09 00:57:02 +0200

    sinle-band

--- dev/js/bands.js
+++ dev/js/bands.js
@@ -3,49 +3,79 @@ let boxImg = document.querySelector('.box-img');
 let bandsImg = document.querySelectorAll('.bands-img');
 let navBg = document.querySelector('.nav-bg');
 let genre = document.querySelectorAll('.genre');
-let bandTyped = document.querySelectorAll('.band-typed')
+let bandTyped = document.querySelectorAll('.band-typed');
 
 let a = 0;
-
-let bandTab = []
-
-let nameBox = ''
+let bandTab = [];
+let nameBox = '';
 
 function nameOver(param) {
-    bandTyped[0].innerHTML = ''
-    bandTab = []
-    a = param;
 
-    nameBox += bandNames[a].innerHTML
-    console.log(nameBox);
+    nameBox += bandNames[param].innerHTML;
 
-    bandsImg[a].style.opacity = "1";
-    bandNames[a].style.transition = ".3s";
-    bandNames[a].style.paddingLeft = "10px";
-
+    bandsImg[param].style.opacity = "1";
+    bandNames[param].style.transition = ".3s";
+    bandNames[param].style.paddingLeft = "10px";
+    
     typed();
 }
 
 function nameOut(param) {
-    a = param;
-    nameBox = ''
-    bandsImg[a].style.opacity = "0";
-    bandNames[a].style.paddingLeft = "0px";
 
-    bandTab = []
-    bandTyped[0].innerHTML = ''
+    nameBox = '';
 
+    bandsImg[param].style.opacity = "0";
+    bandNames[param].style.paddingLeft = "0px";
 }
 
-
 function typed() {
-    bandTab = nameBox.split('')
-    let n = 0
+
+    bandTyped[0].innerHTML = '';
+    bandTab = nameBox.split('');
+
+    n = 0;
+
     setInterval(() => {
         if (n < bandTab.length) {
             bandTyped[0].innerHTML += bandTab[n];
             n++;
         }
     }, 200);
-    clearInterval();
+}
+
+// Single Band
+
+let sbBtn = document.querySelectorAll('.sb-btn-info');
+let sbTxt = document.querySelectorAll('.sb-txt');
+
+function active(x) {
+        if (sbBtn[x].classList.contains('sb-active')) {
+            
+        } else {
+            sbBtn.forEach(element => {
+                element.classList.remove('sb-active')
+            });
+
+            sbBtn[x].classList.add('sb-active')
+        }
+
+            sbTxt.forEach(element => {
+                element.classList.remove('sb-show')
+                element.classList.add('sb-hide')
+            });
+
+        
+        if (sbBtn[x].classList.contains('sb-bio')) {
+            sbTxt[0].classList.add('sb-show');
+            sbTxt[0].classList.remove('sb-hide');
+        } else if (sbBtn[x].classList.contains('sb-releases')) {
+            sbTxt[1].classList.add('sb-show');
+            sbTxt[1].classList.remove('sb-hide');
+        } else if (sbBtn[x].classList.contains('sb-event')) {
+            sbTxt[2].classList.add('sb-show');
+            sbTxt[2].classList.remove('sb-hide');
+        } else if (sbBtn[x].classList.contains('sb-members')) {
+            sbTxt[3].classList.add('sb-show');
+            sbTxt[3].classList.remove('sb-hide');
+        } 
 }
